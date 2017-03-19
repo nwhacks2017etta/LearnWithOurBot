a3ef513778c576614df8ddf234e6310af0bd1662
austinl9 <acm.liu11@gmail.com>
2017-03-18 23:06:41 -0700

    done youtube yolo whatever

--- helpers/youtubeVideo.js
+++ helpers/youtubeVideo.js
@@ -1,6 +1,6 @@
 const FACEBOOK_ACCESS_TOKEN = 'EAADNbvWjXEkBAKKNADN9abtlNDiY5jbwlwANCcVfHjGIrHqb4LzlfWPNouwolvMzPIZBSefr8udQkMZC33Fyt3PgDBa5DEhtZBA7SFWT00qyTZAarNrCLJBLC6sH7H2pYpa8bPAQ3bVeJaiuzOJVB5CEIUE21HU4tZAK5BSSG2wZDZD';
 const DOGE_IMAGE_URL = 'https://upload.wikimedia.org/wikipedia/en/5/5f/Original_Doge_meme.jpg';
-const watchYoutubeUrl = "https://www.youtube.com/watch?v=";
+const watchYoutubeUrl = 'https://www.youtube.com/watch?v=';
 
 const request = require('request');
 const YouTube = require('youtube-node');
@@ -40,14 +40,14 @@ module.exports = (event) => {
                     if (i == 0) {
                         l1Title = item.snippet.title;
                         l1ImageUrl = item.snippet.thumbnails.default.url;
-                        l1ActionUrl = item.id.videoId
-                        console.log(l1Title + " " + l1ImageUrl + " " + l1ActionUrl);
+                        l1VideoUrl = item.id.videoId
+                        console.log(l1Title + " " + l1ImageUrl + " " + l1VideoUrl);
                     }
                     if (i == 1) {
                         l2Title = item.snippet.title;
                         l2ImageUrl = item.snippet.thumbnails.default.url;
-                        l2ActionUrl = item.id.videoId;
-                        console.log(l2Title + " " + l2ImageUrl + " " + l2ActionUrl);
+                        l2VideoUrl = item.id.videoId;
+                        console.log(l2Title + " " + l2ImageUrl + " " + l2VideoUrl);
                     }
                 }
             }
@@ -66,28 +66,39 @@ module.exports = (event) => {
                                     {
                                         "title": l1Title,
                                         "image_url": l1ImageUrl,
-                                        // "default_action": {
-                                        //     "type": "web_url",
-                                        //     "url": "https://diario.mx/Local/2017-01-05_f62a689b/sin-saber-vivio-casi-un-mes-entre-muertos/",
-                                        //     "messenger_extensions": true,
-                                        //     "webview_height_ratio": "tall",
-                                        //     "fallback_url": "https://diario.mx/"
-                                        // }
+                                        "subtitle": watchYoutubeUrl + l1VideoUrl
                                     },
                                     {
-                                         "title": l2Title,
+                                        "title": l2Title,
                                         "image_url": l2ImageUrl,
-                                        // "default_action": {
-                                        //     "type": "web_url",
-                                        //     "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
-                                        //     "messenger_extensions": true,
-                                        //     "webview_height_ratio": "tall",
-                                        //     "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                                        // }
+                                        "subtitle": watchYoutubeUrl + l2VideoUrl
                                     }
                                 ]
                             }
                         }
+                    }
+                }
+            });
+            request({
+                url: 'https://graph.facebook.com/v2.6/me/messages',
+                qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+                method: 'POST',
+                json: {
+                    recipient: { id: senderId },
+                    message: {
+                        text: watchYoutubeUrl + l1VideoUrl,
+                    }
+                }
+            });
+
+             request({
+                url: 'https://graph.facebook.com/v2.6/me/messages',
+                qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+                method: 'POST',
+                json: {
+                    recipient: { id: senderId },
+                    message: {
+                        text: watchYoutubeUrl + l2VideoUrl,
                     }
                 }
             });
