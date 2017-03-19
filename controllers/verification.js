bffa3da2c9b7f191e3a4593af7803e4b355e2d91
tchutw <tchutw@alumni.ubc.ca>
2017-03-18 18:18:46 -0700

    testing changes

--- controllers/verification.js
+++ controllers/verification.js
@@ -2,13 +2,13 @@ module.exports = (req, res) => {
     const hubChallenge = req.query['hub.challenge'];
 
     const hubMode = req.query['hub.mode'];
-    const verifyTokenMatches = (req.query['hub.verify_token'] === 'edi is cool');
+    const verifyTokenMatches = (req.query['hub.verify_token'] === 'verify_token');
 
     if(hubMode && verifyTokenMatches) {
         res.status(200).send(hubChallenge)
     }
 
     else {
-        res.status(403).end();
+        res.status(200).send("Hello world")
     }
 }
