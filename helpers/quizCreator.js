92cd665fccfba8986c03c74a1426153c51d5b095
austinl9 <acm.liu11@gmail.com>
2017-03-19 04:21:54 -0700

    got familiarity level

--- helpers/quizCreator.js
+++ helpers/quizCreator.js
@@ -16,27 +16,42 @@ var config = {
 firebase.initializeApp(config);
 
 var ref = firebase.app().database().ref();
-ref.once("value")
- .then(function (snap) {
- console.log(snap.val());
- });
 
  //need to get the familiarity of the user
 var quizTakers = firebase.app().database().ref("QuizTakers");
-quizTakers.orderByKey().on("child_added", function(snapshot) {
-  console.log(snapshot.key);
+quizTakers.orderByKey().on("value", function(snapshot) {
+  snapshot.forEach(function(data) {
+    console.log("The " + data.key + " dinosaur's score is " + data.val());
+  });
 });
 
  //need to get the question list
-var quizTakers = firebase.app().database().ref("QuizQuestions");
-quizTakers.orderByKey().on("child_added", function(snapshot) {
+var quizQuestions = firebase.app().database().ref("QuizQuestions");
+quizQuestions.orderByKey().on("child_added", function(snapshot) {
   console.log(snapshot.key);
+  console.log(snapshot.value);
 });
 
 
 module.exports = (event) => {
     const senderId = event.sender.id;
     const message = event.message.text;
+
+    var familiarLevel;
+    //familiarity level
+    quizTakers.orderByKey().on("value", function(snapshot) {
+        snapshot.forEach(function(data) {
+            if(data.key == senderId){
+                var familiarLevel = data.val();
+            }
+        });
+
+        //it doesn't exist in the database
+        //we can add a new value later?
+        if(!familiarLevel){
+            familiarLevel = 0;
+        }
+        
 
     var questionList = [];
 
